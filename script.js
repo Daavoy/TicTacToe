d22772dd98a8ca77aa4f3491f6e1da4cf68d32a5
Daavoy <jan-petter.daavoy@outlook.com>
2023-08-22 20:14:27 +0200

    add preliminary placement mechanics

--- script.js
+++ script.js
@@ -4,21 +4,66 @@ const BoardContainer = document.querySelector(".board-container")
 
 
 const GameBoard = (() => {
-    const board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+    const board = new Array(9)
 
     return { board }
 })();
 
 const Player = (name, charater) => {
-    const getName = () => name;
-    const getCharacter = () => charater;
-    return { getName, getCharacter }
+
+    return { name, charater }
 }
 
 const GameContoller = (() => {
 
+    const startGame = () => {
+
+        const gameBoard = GameBoard.board;
+        populateBoard(gameBoard);
+        const playerOne = Player("One", 'X');
+        const playerTwo = Player("Two", 'O')
+        const player = playerOne
+        console.log(`GameBoard ${gameBoard}`)
+        while (!checkForWin(gameBoard)) {
+            makeMove(player, gameBoard);
+
+        }
+    }
+    return { startGame }
 })();
 
+
+function startGame() {
+
+    const gameBoard = GameBoard.board;
+    populateBoard(gameBoard);
+    const playerOne = Player("One", 'X');
+    const playerTwo = Player("Two", 'O')
+    const player = playerOne
+    console.log(`GameBoard ${gameBoard}`)
+
+    makeMove(player, gameBoard);
+
+
+}
+startGame()
+
+
+function makeMove(player, board) {
+    BoardContainer.addEventListener(("click"), function (event) {
+
+        console.log(player.getCharacter)
+        if (event.target.classList.contains("board-square")) {
+            const squareVal = event.target
+            if (squareVal.textContent === 'O' || squareVal.textContent === 'X') {
+                console.log("Invalid placement")
+            }
+            board[squareVal.getAttribute("data-value")] = player.charater
+
+            squareVal.textContent = player.charater
+        }
+    })
+}
 
 function checkForWin(board) {
     return horizontalWin(board) || verticalWin(board) || diagonalWin(board)
@@ -41,31 +86,26 @@ function diagonalWin(board) {
 }
 
 
-function populateBoard() {
-
-    let gameBoard = GameBoard.board;
-
-    for (let i = 0; i < 10; i++) {
-
-        createBoardSquare(gameBoard[i], i);
+function populateBoard(gameBoard) {
+    for (let i = 0; i < 9; i++) {
+        console.log(i);
+        gameBoard[i] = '';
+        createBoardSquare('', i);
     }
 }
 
-populateBoard()
-function createBoardSquare(x, n) {
+
+function createBoardSquare(x, i) {
     const boardSquare = document.createElement("div");
     boardSquare.classList = "board-square"
     boardSquare.textContent = x;
+    boardSquare.setAttribute("data-value", i);
     BoardContainer.appendChild(boardSquare);
 }
 
-BoardContainer.addEventListener(("click"), function (event) {
-    if (event.target.classList.contains("board-square")) {
-        console.log(event.target.textContent)
-    }
-})
 
 
-console.log(isGameOver(['x', 'o', 'o',
+
+console.log(checkForWin(['x', 'o', 'o',
     'o', 'o', 'x',
     'x', 'x', 'o']))
